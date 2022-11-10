b017a7c0bbe04c8b58bba87dd9fd911605506042
LeandroLuethi <92726813+LeandroLuethi@users.noreply.github.com>
2022-11-10 23:22:05 +0100

    Einlesen einer Matrix

--- src/matrizen/MatrixOperations.java
+++ src/matrizen/MatrixOperations.java
@@ -10,21 +10,36 @@ public class MatrixOperations {
 
     public static void readMatrix() throws FileNotFoundException {
             Scanner fileReader = new Scanner(new File(MatrixOperations.PATH_NAME));
+            //Zählen der Anzahl Zeilen
+            int rows=0;
+            while(fileReader.hasNext()){
+                rows++;
+            }
 
-            int[][] matrix = new int[2][3];
-            int line = 1;
+            //Erzeugen eines 2D-Arrays mit der richtigen Grösse.
+            int[][] matrix = new int[rows][fileReader.nextLine().length()];
 
+            //Hilfsvariable, welche angibt, welche Zeile wir gerade einlesen
+            int rowcounter = 1;
+
+            //Schleife geht jede Zeile durch
             while (fileReader.hasNext()) {
-                String nextLine = fileReader.nextLine();
-                for(int i=0; i<nextLine.length();i++){
-                    ArrayList<Character> oneLine = new ArrayList<>();
-                    if(nextLine.charAt(i)  != ' '){
-                        oneLine.add(nextLine.charAt(i));
-                    }
-                        matrix[line][i] = nextLine.charAt(i);
 
+                //liest Zeile ein
+                String nextLine = fileReader.nextLine();
+
+                /*Schleife geht jedes Zeichen der Zeile durch,
+                wenn kein Leerzeichen vorhanden ist, wird das Zeichen in den nächsten freien Arrayslot
+                der Zeile gespeichert. Gaps werden gezählt, um keine leeren Stellen im Array zu haben*/
+                int gaps = 0;
+                for(int i=0; i<nextLine.length();i++){
+                    if(nextLine.charAt(i)  != ' '){
+                        matrix[rowcounter][i-gaps] = Integer.parseInt(String.valueOf(nextLine.charAt(i)));
+                    } else{
+                        gaps++;
+                    }
                 }
-                line++;
+                rowcounter++;
             }
     }
 
