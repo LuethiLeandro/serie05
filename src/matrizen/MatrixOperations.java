3c811d573f7551678cf0be973fe65da602cb634f
LeandroLuethi <92726813+LeandroLuethi@users.noreply.github.com>
2022-11-11 22:55:57 +0100

    still problems

--- src/matrizen/MatrixOperations.java
+++ src/matrizen/MatrixOperations.java
@@ -2,48 +2,51 @@ package matrizen;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MatrixOperations {
 
-    public static int[][] readMatrix(String path) throws FileNotFoundException {
+    public static int[][] readMatrix(String path)
+            throws FileNotFoundException {
             Scanner fileReader = new Scanner(new File(path));
             //Zählen der Anzahl Zeilen und der längsten Zeile
             int rows=0;
             int longLine=0;
             while(fileReader.hasNext()){
-                if(longLine<fileReader.nextLine().length()){
-                    longLine=fileReader.nextLine().length();
+                String line = fileReader.nextLine();
+                int einträgeLine = 2;
+                for(int i = 0; i < line.length(); i++){
+                    if (line.charAt(i)==' '){
+                        einträgeLine++  ;
+                    }
                 }
+                if (einträgeLine > longLine){
+                    longLine = einträgeLine;
+                };
                 rows++;
             }
 
-            //Erzeugen eines 2D-Arrays mit der richtigen Grösse.
-            int[][] matrix = new int[rows][longLine];
-
+            //Instanzierung eines 2D-Arrays mit der richtigen Grösse.
+            int[][] matrix = new int[rows][longLine-1];
+             Scanner fileReader2 = new Scanner(new File(path));
             //Hilfsvariable, welche angibt, welche Zeile wir gerade einlesen
             int rowcounter = 1;
             //Schleife geht jede Zeile durch
-            while (fileReader.hasNext()) {
+            while (fileReader2.hasNext()) {
 
                 //liest Zeile ein
-                String nextLine = fileReader.nextLine();
+                String nextLine = fileReader2.nextLine();
 
                 /*Schleife geht jedes Zeichen der Zeile durch,
                 wenn das Zeichen kein Leerzeichen ist, wird es in den Array nextNumber gespeichert.
                 Falls ein Leerzeichen vorhanden ist, wird in den richtigen Arrayslot die Zahl gespeichert,
                 der String nectNumber wird reseted und gaps wird erhöht.*/
                 int gaps = 0;
-                String nextNumber = "";
-                for(int i=0; i<nextLine.length();i++){
-                    if(nextLine.charAt(i)  != ' '){
-                        nextNumber =""+nextLine.charAt(i);
-                    } else{
-                        matrix[rowcounter][gaps+1] = Integer.parseInt(String.valueOf(nextNumber));
-                        nextNumber="";
-                        gaps++;
-                    }
+                Scanner lineScanner = new Scanner(nextLine);
+                lineScanner.useDelimiter(" ");
+                while (lineScanner.hasNext()) {
+                      matrix[rowcounter][gaps] = Integer.parseInt(lineScanner.next());
                 }
                 rowcounter++;
             }
--- src/matrizen/MatrixTest.java
+++ src/matrizen/MatrixTest.java
@@ -1,7 +1,11 @@
 package matrizen;
 
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 public class MatrixTest {
-    public static void main(String[] args) {
-        System.out.println(readMatrix("src/matrizen/matrix1.txt"));
+    public static void main(String[] args)
+            throws FileNotFoundException {
+        System.out.println(Arrays.deepToString(MatrixOperations.readMatrix("src/matrizen/matrix1.txt")));
     }
 }
