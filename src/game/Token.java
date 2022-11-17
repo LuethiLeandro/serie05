5a781d60882c7f59075a67bdb0aa4c8ac2c5ba0b
LeandroLuethi <92726813+LeandroLuethi@users.noreply.github.com>
2022-11-17 18:48:00 +0100

    ex.1 computerplayer

---
+++ src/game/Token.java
@@ -0,0 +1,18 @@
+package game;/* ************************************************************************* *\
+*                Programmierung 1 HS 2020 - Serie 5-1                         * 
+\* ************************************************************************* */
+
+//package habe ich reingemacht
+
+
+/**
+ * Basis enum to provide player tokens
+ */
+public enum Token {
+       empty( " " ),
+       player1( "O" ),
+       player2( "X" );
+       private String representation; // string representation of value
+       Token( String s ) { this.representation = s; }
+       public String toString() { return this.representation; }
+}
